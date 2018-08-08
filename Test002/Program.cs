f7f766978b9445f483452f5c5589587a6caf7dfe
Kohashi Akio <kohashi.akio@jaea.go.jp>
2018-08-08 15:54:59 +0900

    プロジェクト作成

--- Test002/Program.cs
+++ Test002/Program.cs
@@ -10,6 +10,8 @@ namespace Test002
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello GitHub!");
+            Console.ReadKey(true);
         }
     }
 }
